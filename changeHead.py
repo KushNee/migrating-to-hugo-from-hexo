06357766e4198b1429d03441e2c9d9ec4889b0c7
KushNee <wxtz521@gmail.com>
2016-01-21 09:50:24 +0800

    Init, still many bugs

---
+++ changeHead.py
@@ -0,0 +1,46 @@
+import os
+import re
+
+
+def changeEquall(StringTochange=''):
+    elements = re.split(r': |\n', StringTochange)
+    print(elements)
+    newString = elements[0]+' = "'+elements[1]+'"'+'\n'
+    return newString
+
+targetDir = r'/Users/kushnee/kcn_hugo/content/post'
+b = os.listdir(targetDir)
+for index in range(0, len(b)):
+    f = open(targetDir+'/'+b[index], 'r')
+    now = f.readlines()
+    f.close()
+    #print('pre_now[0]: '+now[0])
+    if now[0] != '+++\n':
+        now[0] = '+++\n' + now[0]
+        #print('current_now[0]: {}'.format(now[0]))
+    for i in range(0, 5):
+        if re.search('title', now[i]):
+            if now[i].find(': '):
+                if i != 0:
+                    #print('Find title')
+                    now[i] = changeEquall(now[i])
+                    #print('current_now[{}]: {}'.format(i, now[i]))
+                else:
+                    elements = re.split(r': |\n', now[i])
+                    #print(now[i])
+                    now[i] = elements[0]+'\n'+elements[1]+' = "'+elements[2]+'"\n'
+        elif re.search('date', now[i]):
+            if now[i].find(': '):
+                if i != 0:
+                    #print('Find date')
+                    now[i] = changeEquall(now[i])
+                    #print('current_now[{}]: {}'.format(i, now[i]))
+                else:
+                    elements = re.split(r': |\n', now[i])
+                    #print(now[i])
+                    now[i] = elements[0]+'\n'+elements[1]+' = "'+elements[2]+':\n'
+    with open(targetDir+'/'+b[index], 'w') as f2:
+        f2.writelines(now)
+print('All Done')
+
+
